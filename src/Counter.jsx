48160670dd29b1b293bb226d7096cc790ad4c131
Noah Humphreys <noahlorinhumphreys@gmail.com>
2022-10-25 21:00:51 -0600

    up to date

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,12 +1,33 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
+import { ThemeContext } from './App'
 
-export default function Counter({initialCount}) {
-    const [count, setCount] = useState(initialCount)
-  return (
-    <>
-      <button onClick={() => setCount(prevCount => prevCount +1)}>+</button>
-      <span>{count}</span>
-      <button onClick={() => setCount(prevCount => prevCount -1)}>-</button>
-    </>
-  )
+export default class Counter extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: props.initialCount
+    }
+  }
+
+  render() {
+    console.log("Render Counter")
+    return (
+      <ThemeContext.Consumer>
+        {style => (
+          <div>
+            <button style={style} onClick={() => this.changeCount(-1)}>-</button>
+            <span>{this.state.count}</span>
+            <button style={style} onClick={() => this.changeCount(1)}>+</button>
+          </div>
+        )}
+      </ThemeContext.Consumer>
+    )
+  }
+
+  changeCount(amount) {
+    this.setState(prevState => { 
+      return { count: prevState.count + amount }
+    })
+  }
 }
